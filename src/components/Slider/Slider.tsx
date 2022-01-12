3e654bb4efbcddb15564d4f267d094af32c65ec9
salesruby <administrator@salesruby.com>
2022-01-12 09:51:00 +0100

    changed slide counts

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -11,7 +11,7 @@ export default function SimpleSlider({ Component, data }: SliderProp) {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true
   };
