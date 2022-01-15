f76734ce5038ab8f788c3162ff69cde4a53d1939
salesruby <administrator@salesruby.com>
2022-01-15 14:14:36 +0100

    Navlink

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from '../../SOAIcon/Icon';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
