7c28c9d5c50716c69f5fb1bacf1c98e5eb8a21cd
salesruby <administrator@salesruby.com>
2022-01-29 21:55:47 +0100

    image size

--- src/pages/Mint/Mint.tsx
+++ src/pages/Mint/Mint.tsx
@@ -3,38 +3,50 @@ import { RiTelegramLine, RiDiscordLine, RiTwitterLine } from 'react-icons/ri';
 
 function Mint() {
   return (
-    <div className='row spirits'>
-      <div className='col-sm-8'>
-        <div className='spirit-intro'>spirits of</div>
-        <div className='spirit-intro2'>africa</div>
-        <div className='mt-3'>
-          <a href='/#' target='_blank'>
+    <div className="row spirits">
+      <div className="col-sm-8">
+        <div className="spirit-intro">spirits of</div>
+        <div className="spirit-intro2">africa</div>
+        <div className="mt-3">
+          <a
+            href="https://t.me/Spiritsofafrica"
+            rel="noreferrer"
+            target="_blank"
+          >
             <RiTelegramLine
               style={{ fontSize: '3rem', color: 'var(--primary-color)' }}
             />
           </a>
-          <a href='/#' target='_blank'>
+          <a
+            href="https://www.twitter.com/Spirits0fAfrica"
+            rel="noreferrer"
+            target="_blank"
+          >
             {' '}
             <RiDiscordLine
               style={{
                 fontSize: '3rem',
                 color: 'var(--primary-color)',
-                marginLeft: '1rem'
+                marginLeft: '1rem',
               }}
             />
           </a>
-          <a href='/#' target='_blank'>
+          <a
+            href="https://www.twitter.com/Spirits0fAfrica"
+            rel="noreferrer"
+            target="_blank"
+          >
             <RiTwitterLine
               style={{
                 fontSize: '3rem',
                 color: 'var(--primary-color)',
-                marginLeft: '0.3rem'
+                marginLeft: '0.3rem',
               }}
             />
           </a>
         </div>
       </div>
-      <div className='col-sm-4'></div>
+      <div className="col-sm-4"></div>
     </div>
   );
 }
