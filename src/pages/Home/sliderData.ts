a2b8c35ba57c69b685d7bda7b75362f9e62b2fa5
salesruby <administrator@salesruby.com>
2022-01-29 15:15:41 +0100

    adding more images

--- src/pages/Home/sliderData.ts
+++ src/pages/Home/sliderData.ts
@@ -1,9 +1,10 @@
 import soa1 from '../../assets/images/soa1.jpeg'
 import soa2 from '../../assets/images/soa2.jpeg'
 import soa3 from '../../assets/images/soa3.jpeg'
-
-
-
+import soa4 from '../../assets/images/soa4.jpeg'
+import soa5 from '../../assets/images/soa5.jpeg'
+import soa6 from '../../assets/images/soa6.jpeg'
+import soa7 from '../../assets/images/soa7.jpeg'
 
 export const sliderData = [
 {id:'1',image:soa1,
@@ -16,7 +17,13 @@ url: '/' },
 		url: '/' },
 		{id:'4',
 		image:soa2},
-		{id:'3',
-	image:soa1,
+		{id:'5',
+	image:soa5,
 	url: '/' },
+	{id:'6',
+	image:soa6,
+	url: '/' },
+	{id:'7',
+	image:soa7,}
+
 ]
