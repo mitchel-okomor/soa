e6b83f3dae014e6a6b25a86a509edcdd09edfff3
salesruby <administrator@salesruby.com>
2022-01-30 20:20:17 +0100

    nav bar

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -4,51 +4,53 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light bg-light top-navbar'>
-      <div className='container-fluid'>
-        <Link className='navbar-brand' to='/'>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light top-navbar">
+      <div className="container-fluid">
+        <Link className="navbar-brand mx-2" to="/">
           <Icon />
         </Link>
-        {/* <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarText'
-          aria-controls='navbarText'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className='navbar-toggler-icon'></span>
+          <span className="toggler-icon top-bar"></span>
+          <span className="toggler-icon middle-bar"></span>
+          <span className="toggler-icon bottom-bar"></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarText'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item '>
-              <a className='nav-link' href='#mint'>
-                Mint NFT
-              </a>
-            </li>
-            <li className='nav-item mx-2'>
-              <a className='nav-link' href='#collections'>
-                Collection
-              </a>
-            </li>
-            <li className='nav-item mx-2'>
-              <a className='nav-link' href='#explore'>
-                Explore NFT
-              </a>
-            </li>
-            <li className='nav-item mx-2'>
-              <a className='nav-link' href='#about'>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mx-2 ">
+              <a className="nav-link " href="#mint">
                 About
               </a>
             </li>
+            <li className="nav-item mx-2">
+              <a className="nav-link" href="#collections">
+                RoadMap
+              </a>
+            </li>
+            <li className="nav-item mx-2">
+              <a className="nav-link" href="#explore">
+                FAQ
+              </a>
+            </li>
+            {/* <li className="nav-item mx-2">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li> */}
           </ul>
-          <div className='right mx-4'>
-            <Link className='pagebtn' to='/login'>
+          {/* <div className="right mx-4">
+            <Link className="pagebtn" to="/login">
               Login
             </Link>
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </div>
     </nav>
   );
