57a2eac1f3b6e35f2238365ca969e80e864d9a1d
salesruby <administrator@salesruby.com>
2022-02-02 13:03:23 +0100

    goal texts

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -17,22 +17,22 @@ function About() {
         <p>
           Spirits of Africa is an NFT project inspired by bringing Africa closer
           to the world by showcasing African mythology. SoA was started by a
-          small group of Africans want to educate the world with stories about
-          Africa mythology.
+          small group of Africans that want to educate the world with stories
+          about Africa mythology.
         </p>
         <h3>
           <b>Goals:</b>
         </h3>
         <p>
           African myth is so interesting but does not get the recognition it
-          deserves. Our goal at SoA is to make give the whole world the
-          opportunity to enjoy African myth through NFT's. We have so many
-          beautiful untold and poorly expressed stories about Africa and African
-          myth, that's our inspiration at SoA. We want to be a source of hope to
-          the African Community, helping people all over the world understand
-          and enjoy Africa by making sure that l the world gets to know more
-          about the true African stories, how it happened, when it happened and
-          where it happened.
+          deserves. Our goal at SOA is to give the whole world the opportunity
+          to enjoy African myth through NFT's. We have so many beautiful untold
+          and poorly expressed stories about Africa and African myth, that's our
+          inspiration at SoA. We want to be a source of hope to the African
+          Community, helping people all over the world understand and enjoy
+          Africa by making sure that l the world gets to know more about the
+          true African stories, how it happened, when it happened and where it
+          happened.
         </p>
         <p>
           <b>SOA</b> is a project for the people and will forever support the
